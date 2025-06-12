20f3d7aa0c84d9a96fa3e51e043e92647f011bc9
M4YH3M <mayhem.dev.official@gmail.com>
2025-06-12 19:40:18 +0530

    Add files via upload

--- src/app/welcome/[name]/page.tsx
+++ src/app/welcome/[name]/page.tsx
@@ -1,1 +1,15 @@
+import WelcomeComponent from "@@/src/components/WelcomeComponent";
+import React from "react";
 
+export default async function WelcomePage({
+  params,
+}: {
+  params: Promise<{ name: string }>;
+}) {
+  const { name } = await params;
+  return (
+    <div>
+      <WelcomeComponent name={name} />
+    </div>
+  );
+}
