c1fca63d0a0e2a3847c16324420a5bcdbef7f1fd
M4YH3M <mayhem.dev.official@gmail.com>
2025-06-12 18:25:11 +0530

    Update userList.ts

--- data/userList.ts
+++ data/userList.ts
@@ -1,1 +1,90 @@
+interface UserStructure {
+  name: string;
+  password: string;
+  picLink: string;
+  inviteMessage: string;
+  songLink: string;
+}
 
+export const userList: UserStructure[] = [
+  {
+    name: "Anushka",
+    password: "ckhitler",
+    picLink: "/img/anushka.png",
+    inviteMessage:
+      "FInally aab tumera message, I have been working on this website from 30 hrs, bus ek message likhne ke lie bcccc. Sabka kr die aab tumera bacah hai bus, dekho I understand jo bhi fuckup hua humlogka, tum mere se baat krna band kr di and all..... I guess aab sab sorted hai and aab nhi hoga fuckup paaka promise :) 12th april ko bday aarha hai and tumko bhi ana hai hmm. Pickup and drop kr denge time pe, subha subha he ana hai, waise ek request hai, treditional phen ke ana, tumse aacha lgta hai ;) chalo sone jarai hai aab thak gai kaam krte krte.... bue bue",
+    songLink: "/song/Dupatta.mp3",
+  },
+  {
+    name: "Jigglypuff",
+    password: "pokemon",
+    picLink: "/img/jigglypuff.png",
+    inviteMessage:
+      "Ooi jiggly puff need some stuff???? Dekh 'nhi main nhi aarai bday pe' wala laura lassan nhi chalega samjhi? chup chap aja werna jiggly puff se wiggly tuff kr denge hehehhe. And thanks tu jitna bhi help ki mera, maza ata hai tere ko perashan kr ke heheh, chal bday ke din chup chap neche aajana and lets go according to seating plan IYKYK. Bue bue",
+    songLink: "/song/Jigglypuff.mp3",
+  },
+  {
+    name: "Bouncer",
+    password: "dabangg",
+    picLink: "/img/bouncer.png",
+    inviteMessage:
+      "Dabangg bhen <3, tu bhot pyari hai reee, bus aaj kl jada busy rehti hai hehehe. tum bhot help ki ho mera mere disperation me thankuuuu har time mere sath rehene ke lie. Aab chup chap bday pe aja cake khainge and tasty tasty gher ka chicken yemmmmmmm muh me pani aagya bc sachi, yeh likhte waqt khane ka maan kr rah hai hehehhe jarai kuch khane, bday ke din aajana neche bue bue.",
+    songLink: "/song/dabangg.mp3",
+  },
+  {
+    name: "Sanket",
+    password: "jalkukra",
+    picLink: "/img/sanket.png",
+    inviteMessage:
+      "Bhosdike gand mrate rehta hai, saale jabse mila hai tu bherwe kismat ke laure lge hue hai fir bhi sath hai hum, kyu ki tu bhosdika bhai hai. Love you bherwe, har bar sath me he rehte hai and sath me he rehainge. Chal aab jada hogya tera tarif chup chap aajna bday pe werna tera lash khi kone me feka hua milega samjhe bherwe, bye.",
+    songLink: "/song/bhai hai.mp3",
+  },
+  {
+    name: "Panda",
+    password: "cravings",
+    picLink: "/img/panda.png",
+    inviteMessage:
+      "Panda oh panda hehehehe you craving for anything??? hehehheh tu one of the ogs friend hai 7 8 mehine se sath me hai isslie iitna mehanat kr rai bday pe invite krne ke lie socho hum kitne aache dost hai tumhere. Hopefully aapna trio 8th sem tk chalte rehai <3 chal bday ke din neche aajana gadi leke aajainge lene. bue bue",
+    songLink: "/song/Height Teri Chhoti.mp3",
+  },
+  {
+    name: "HB",
+    password: "gandmara",
+    picLink: "/img/hb.png",
+    inviteMessage:
+      "Bhaiiiii <3 Tori kalai na chorenge hum tore bin hum adhure hai ram kasam...... bhai tu toh bestie hai mera, har hackathon me and technical chiz me sath he rha hai mera buddy. Hum dono humesha sath me rehainge bhai 8th sem and uske baad tk bc, bestfriends hai bhai iitna aasani se thori dosti tutega.... aaja bday pe mere gadi aajaiga neche tu sirf ready hoke uuter jana pick hum kr lenge and chor bhi denge <3. bue bue",
+    songLink: "/song/Tori Kalai na chorenge hum.mp3",
+  },
+  {
+    name: "Kittu",
+    password: "bewadi",
+    picLink: "/img/kittu.png",
+    inviteMessage:
+      "Bhen tu bhot bewadi hai km pia kr, sab try krna hai tere ko lol. Jobhi hai jaisi bhi hai tu meri pyari bhe hai <3 bday aarha hai mera 12 april ko ajana mere bday pe gher me maze krenge, sirf ready rehena gadi lene aajaiga and chor bhi dega bhen <3. bue bue",
+    songLink: "/song/Chhote Chhote Peg.mp3",
+  },
+  {
+    name: "Rewant",
+    password: "badmosh",
+    picLink: "/img/rewant.png",
+    inviteMessage:
+      "Bhai sach baatai tu bhot km time me bhot sahi dost bangya hai bhai <3 love you bherwe, tere driving oomphoooooooo bhai waise drive krne me maza ata hai, pta nhi yeh ladkiyo ko kyu iitna dikket hota hai, mere ko bhot maza ata hai tere sath. Chal bhai bday ke din gadi ready rekhna dopher me he nikalenge and shyad thora ghum bhi le <3 bue bue",
+    songLink: "/song/2khatola.mp3",
+  },
+  {
+    name: "Doraemon",
+    password: "nobitakibiwi",
+    picLink: "/img/doraemon.png",
+    inviteMessage:
+      "Doraemon bhen heheh, bhi tu bhot sweet hai and b hot innocent lgti hai re kasam se, tu  bhot aaise qt patuti types bhen hai, choti si moti si gol matol bhen padhi likhi kr thora and muh kam fulaya kr bhen, baki bday pe toh ana he hai tere ko, ready hoke neche aajana lene aajainge tere ko and time pe pauch abhi denge chal aab sone jarai.. bue bue",
+    songLink: "/song/doraemon.mp3",
+  },
+  {
+    name: "Khushi",
+    password: "gundikhiki",
+    picLink: "/img/khushi.png",
+    inviteMessage:
+      "Bhen tu bhot pyari bhen hai mera, meri sabse pheli bhen, tu bhot naughty harkat krti hai re hawasi khi ki lol. jo bhi bolo pyari toh ho he tum, waise mera bday hai 12th april ko and tu mere bday pe aaigi, lana and chorna hojaiga dont worry tere ko pick and drop kr denge, bus ready hoke neche aajana. chal bue bue.",
+    songLink: "/song/lofar.mp3",
+  },
+];
