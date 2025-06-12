b512ac0d1d31796ec90b79d09c4e07e12d1d4545
M4YH3M <mayhem.dev.official@gmail.com>
2025-06-12 19:06:06 +0530

    Add files via upload

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,1 +1,29 @@
+import type { Metadata } from "next";
+import { Geist_Mono } from "next/font/google";
+import "@/styles/globals.css";
 
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Birthday Invite 2025",
+  description: "Aajana bday me",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body
+        className={`${geistMono.className} antialiased bg-linear-to-br from-[#0A0C0E] via-[#1A1E23] to-[#050607] text-[#F6F9FC] h-screen`} 
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
