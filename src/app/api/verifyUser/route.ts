f6fed23c0cd7a453a3b045fdebd540c0030163f1
M4YH3M <mayhem.dev.official@gmail.com>
2025-06-12 19:09:54 +0530

    Add files via upload

--- src/app/api/verifyUser/route.ts
+++ src/app/api/verifyUser/route.ts
@@ -1,1 +1,29 @@
+import { userList } from "@@/data/userList";
 
+export async function POST(req: Request) {
+  const data = await req.json();
+
+  const user = userList.find(
+    (user) =>
+      user.name.toLowerCase() === data.name.toLowerCase() &&
+      user.password === data.password
+  );
+
+  if (user) {
+    return new Response(
+      JSON.stringify({ 
+        status: 200,
+        user: {
+          name: user.name,
+          picLink: user.picLink,
+          inviteMessage: user.inviteMessage
+        }
+      }), 
+      { status: 200 }
+    );
+  } else {
+    return new Response(JSON.stringify({ status: 401 }), {
+      status: 401,
+    });
+  }
+}
